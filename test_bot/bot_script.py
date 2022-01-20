179ce9341e1783d5c8cc9f4da29b94ab3b708d4d
Ftp-Glich <85408465+Ftp-Glich@users.noreply.github.com>
2022-01-21 00:08:01 +0300

    Update bot_script.py

--- test_bot/bot_script.py
+++ test_bot/bot_script.py
@@ -1,12 +1,12 @@
 import telebot
 
 from searcher import Searcher
+from users_base import UserBase
 from telebot import types
 
 from requests import get
 
-search_list = []
-
+users_base = UserBase()
 searcher = Searcher()
 
 
@@ -15,7 +15,7 @@ def clear_list(arr):
         arr.pop()
 
 
-def append_to_list(el):
+def append_to_list(el, search_list):
     for element in search_list:
         if el == element:
             return False
@@ -64,8 +64,6 @@ for i in range(7):
 buttons_types.append([])
 buttons_types[7].append(types.InlineKeyboardButton(genres[7][0], callback_data=genres[7][0]))
 buttons_types[7].append(types.InlineKeyboardButton(genres[7][1], callback_data=genres[7][1]))
-mess = []
-results1 = []
 
 
 def show_res(results, call):
@@ -78,11 +76,11 @@ def show_res(results, call):
     if len(results) == 0 or len(results) <= 21:
         bot.send_message(chat_id=call.message.chat.id,
                          text="Мы вывели все результаты")
-        call.data = platform[0]
+        call.data = "Давай попробуем"
         call_back(call)
         return 0
 
-    results = reindex_res(results)
+    reindex_res(results)
     keyboard = list()
     row1 = list()
     row1.append(types.InlineKeyboardButton("Ещё", callback_data="more"))
@@ -101,47 +99,68 @@ def ask_platform(call):
     Какое у вас устройство?", reply_markup=markup)
 
 
-is_first_usage = [True]
-platform = [""]
-
-
 @bot.callback_query_handler(func=lambda call: True)
 def call_back(call):
     mes = "Вы пока ничего не выбрали"
     if call.data == "Давай попробуем":
-        if is_first_usage[0]:
+        print("1")
+        platform = users_base.get_platform(call.from_user.id)
+        print("1")
+        if platform == 0:
             ask_platform(call)
+        else:
+            print("2")
+            users_base.clear_search_list(call.from_user.id)
+            keyboard = []
+            print("3")
+            if platform == "mobile":
+                for o in range(8):
+                    for h in buttons_types[o]:
+                        keyboard.append([h])
+            else:
+                for o in range(8):
+                    keyboard.append(buttons_types[o])
+            print("4")
+            murkup = types.InlineKeyboardMarkup(keyboard)
+            print("5")
+            bot.send_message(call.message.chat.id, "Выберите жанры: ", reply_markup=murkup)
+            print("6")
+            temp = bot.send_message(call.message.chat.id, mes)
+            print("7")
+            users_base.set_message_id(call.from_user.id, temp.id)
+            print("8")
+            users_base.set_chat_id(call.from_user.id, call.message.chat.id)
+            print("9")
+
+    elif call.data == "mobile" or call.data == "desktop":
+        users_base.add_user(call.from_user.id, call.data)
+        users_base.clear_search_list(call.from_user.id)
+        platform = call.data
+        users_base.clear_search_list(call.from_user.id)
+        keyboard = []
+        if platform == "mobile":
+            for o in range(8):
+                for h in buttons_types[o]:
+                    keyboard.append([h])
+        else:
+            for o in range(8):
+                keyboard.append(buttons_types[o])
+        murkup = types.InlineKeyboardMarkup(keyboard)
+        bot.send_message(call.message.chat.id, "Выберите жанры: ", reply_markup=murkup)
+        bot.send_message(call.message.chat.id, mes)
+        users_base.set_message_id(call.from_user.id, call.message.message_id)
+        users_base.set_chat_id(call.from_user.id, call.message.chat.id)
     elif call.data == "Очистить":
-        for o in range(len(search_list) - 1):
-            clear_list(search_list)
+        users_base.clear_search_list(call.from_user.id)
         mes = "Вы пока ничего не выбрали"
-        mess[0] = bot.edit_message_text(chat_id=mess[0].chat.id, message_id=mess[0].message_id,
-                                        text=mes)
-    elif call.data == "mobile":
-        platform[0] = call.data
-        clear_list(search_list)
-        keyboard = []
-        for o in range(8):
-            for h in buttons_types[o]:
-                keyboard.append([h])
-        murkup = types.InlineKeyboardMarkup(keyboard)
-        bot.send_message(call.message.chat.id, "Выберите жанры: ", reply_markup=murkup)
-        mess.append(bot.send_message(call.message.chat.id, mes))
-    elif call.data == "desktop":
-        platform[0] = call.data
-        clear_list(search_list)
-        keyboard = []
-        for o in range(8):
-            keyboard.append(buttons_types[o])
-        murkup = types.InlineKeyboardMarkup(keyboard)
-        bot.send_message(call.message.chat.id, "Выберите жанры: ", reply_markup=murkup)
-        mess.append(bot.send_message(call.message.chat.id, mes))
+        bot.edit_message_text(chat_id=users_base.get_chat_id(call.from_user.id),
+                              message_id=users_base.get_message_id(call.from_user.id), text=mes)
     elif call.data == "more":
-        reindex_res(results1[0])
-        show_res(results1[0], call)
+        users_base.set_results(call.from_user.id, reindex_res(users_base.get_results(call.from_user.id)))
+        show_res(users_base.get_results(call.from_user.id), call)
     elif call.data == "again":
-        clear_list(results1[0])
-        call.data = platform[0]
+        users_base.clear_results(call.from_user.id)
+        call.data = "Давай попробуем"
         call_back(call)
 
     elif call.data != "Поиск":
@@ -153,20 +172,17 @@ def call_back(call):
                     break
         if not temp:
             bot.delete_message(call.message.chat.id, call.chat.message_id)
-        append_to_list(call.data)
+        if not append_to_list(call.data, users_base.get_search_list(call.from_user.id)):
+            users_base.append_to_search_list(call.from_user.id, call.data)
         mes = ""
-        for p in range(len(search_list)):
-            mes += search_list[p] + " "
-        mess[0] = bot.edit_message_text(chat_id=mess[0].chat.id, message_id=mess[0].message_id, text="Вы выбрали:\
- " + mes)
+        for p in users_base.get_search_list(call.from_user.id):
+            mes += p + " "
+        bot.edit_message_text(chat_id=users_base.get_chat_id(call.from_user.id),
+                              message_id=users_base.get_message_id(call.from_user.id),
+                              text="Вы выбрали:" + mes)
     elif call.data == "Поиск":
-        clear_list(mess)
-        if len(results1) == 0:
-            results1.append((searcher.get_res(search_list)))
-        else:
-            results1[0] = (searcher.get_res(search_list))
-        show_res(results1[0], call)
-        is_first_usage[0] = False
+        users_base.set_results(call.from_user.id, searcher.get_res(users_base.get_search_list(call.from_user.id)))
+        show_res(users_base.get_results(call.from_user.id), call)
 
 
 bot.polling()
